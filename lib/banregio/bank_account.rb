b9055b31404ce29cafc269e7dc06c6b64aaaff23
Abraham Kuri <kurenn@icalialabs.com>
2015-08-25 13:09:20 -0500

    Adds factory for debit and credit account types

--- lib/banregio/bank_account.rb
+++ lib/banregio/bank_account.rb
@@ -5,7 +5,18 @@ module Banregio
 
     def bank_accounts
       response = self.class.get("/accounts")
-      response.map { |attrs| Banregio::Models::BankAccount.new(attrs) }
+      response.map { |attrs| Banregio::Models::BankAccountFactory.build(attrs) }
+    end
+
+    def link_bank_account(client_number, card_details = {})
+      response = self.put("/accounts", body: {
+        client: {
+          number: client_number
+        },
+        card: card_details
+      })
+
+      Banregio::Models::BankAccountFactory.build(response)
     end
   end
 end
--- lib/banregio/models/bank_account.rb
+++ lib/banregio/models/bank_account.rb
@@ -1,17 +1,53 @@
 module Banregio
   module Models
+    class BankAccountFactory
+
+      def self.build(attrs = {})
+        is_credit_card = attrs["is_credit_card"] || false
+
+        {
+          true => CreditBankAccount,
+          false => DebitBankAccount
+        }.fetch(is_credit_card).new(attrs)
+      end
+    end
+
     class BankAccount
-      attr_reader :id, :account_number, :clabe,
-                  :info_description, :info_balance,
-                  :info_symbol
+      attr_reader :id, :is_credit_card, :card_number,
+                  :balance, :account_number
 
       def initialize(attrs = {})
         @id = attrs["id"]
+        @is_credit_card = attrs["is_credit_card"]
+        @card_number = attrs["card_number"]
+        @balance = attrs["balance"]
         @account_number = attrs["account_number"]
+      end
+    end
+
+    class CreditBankAccount < BankAccount
+      attr_reader :limit, :annual_percentage_rate, :closing_date,
+                  :last_closing_date, :statement_balance, :minimum_payment,
+                  :due_date
+
+      def initialize(attrs = {})
+        super(attrs)
+        @limit = attrs["limit"]
+        @annual_percentage_rate = attrs["annual_percentage_rate"]
+        @closing_date = Date.parse(attrs["closing_date"])
+        @last_closing_date = Date.parse(attrs["last_closing_date"])
+        @statement_balance = attrs["statement_balance"]
+        @minimum_payment = attrs["minimum_payment"]
+        @due_date = attrs["due_date"]
+      end
+    end
+
+    class DebitBankAccount < BankAccount
+      attr_reader :clabe
+
+      def initialize(attrs = {})
+        super(attrs)
         @clabe = attrs["clabe"]
-        @info_description = attrs["info"]["description"]
-        @info_balance = attrs["info"]["balance"]
-        @info_symbol = attrs["info"]["symbol"]
       end
     end
   end
